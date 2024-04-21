ce9d5a4049ae0b09c206e0a61584fce9d246d451
muriloPereira006 <muriloedu006@gmail.com>
2024-04-21 20:53:02 -0300

    add: csv converter function

--- backend/src/controller/validations.js
+++ backend/src/controller/validations.js
@@ -1,9 +1,25 @@
-const mock = require("../test/mocks/shopper_products");
+const parseString = (inputString) => {
+  const cleanedString = inputString.replace(/\r/g, '');
 
-const readCSV = async () => {
-  const products = [];
-  return mock
+  const lines = cleanedString.split('\n');
+  const keys = lines[0].split(',');
+  const objects = [];
+
+  for (let i = 1; i < lines.length; i++) {
+    const values = lines[i].split(',');
+    const obj = {};
+    for (let j = 0; j < keys.length; j++) {
+      obj[keys[j]] = values[j];
+    }
+
+    objects.push(obj);
+  }
+  return objects;
 }
+
+const readCSV = async (csvData) => {
+  return parseString(csvData);
+};
 
 const readXLSX = () => { }
 
--- backend/src/test/validations.test.js
+++ backend/src/test/validations.test.js
@@ -1,14 +1,18 @@
 const { readCSV, readXLSX } = require("../controller/validations");
+const fs = require('fs');
+const path = require('path')
 const mock = require("./mocks/shopper_products");
 
 test('Recebe arquivo .csv e retorna um Array de produtos em formato JSON', async () => {
-  const products = await readCSV();
-  expect(products).toBe(mock)
+  const csvFilePath = path.join(__dirname, './mocks/csvdemo.csv');
+  const csvData = fs.readFileSync(csvFilePath, 'utf8');
+  const products = await readCSV(csvData);
+  expect(products).toEqual(mock);
 });
 
 // test('Recebe arquivo .xlsx e retorna um Array de produtos em formato JSON', () => {
 //   const xlsxFile = fs.readFileSync('./mock/shopper_products.xlsx')
-//   const products = readXLSX(xlsxFile)
+//   const products = readXLSX(xlsxFile) 
 //   expect(products).toBe(mock)
 // });
 
