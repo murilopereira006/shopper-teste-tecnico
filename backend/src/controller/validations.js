5fc806e8d30b1ea6e302c67d2dcb39499016167d
muriloPereira006 <muriloedu006@gmail.com>
2024-04-21 19:05:05 -0300

    reset

---
+++ backend/src/controller/validations.js
@@ -0,0 +1,10 @@
+
+import mock from "../test/mocks/shopper_products";
+
+export const readCSV = async () => {
+  const products = [];
+
+  return mock
+}
+
+export const readXLSX = () => { }
---
+++ backend/src/test/mocks/shopper_products.ts
@@ -0,0 +1,24 @@
+interface Product {
+  id: number,
+  code: number,
+  name: string,
+  cost_price: number,
+  sales_price: number
+}
+
+const mock: Product[] = [
+  { id: 1, code: 16, name: 'AZEITE PORTUGUS EXTRA VIRGEM GALLO 500ML', cost_price: 18.44, sales_price: 20.49 },
+  { id: 2, code: 18, name: 'BEBIDA ENERGTICA VIBE 2L', cost_price: 8.09, sales_price: 8.99 },
+  { id: 3, code: 19, name: 'ENERGTICO  RED BULL ENERGY DRINK 250ML', cost_price: 6.56, sales_price: 7.29 },
+  { id: 4, code: 20, name: 'ENERGTICO RED BULL ENERGY DRINK 355ML', cost_price: 9.71, sales_price: 10.79 },
+  { id: 5, code: 21, name: 'BEBIDA ENERGTICA RED BULL RED EDITION 250ML', cost_price: 10.71, sales_price: 11.71 },
+  { id: 6, code: 22, name: 'ENERGTICO  RED BULL ENERGY DRINK SEM ACAR 250ML', cost_price: 6.74, sales_price: 7.49 },
+  { id: 7, code: 23, name: 'GUA MINERAL BONAFONT SEM GS 1,5L', cost_price: 2.15, sales_price: 2.39 },
+  { id: 8, code: 24, name: 'FILME DE PVC WYDA 28CMX15M', cost_price: 3.59, sales_price: 3.99 },
+  { id: 9, code: 26, name: 'ROLO DE PAPEL ALUMNIO WYDA 30CMX7,5M', cost_price: 5.21, sales_price: 5.79 },
+  { id: 10, code: 1000, name: 'BEBIDA ENERGTICA VIBE 2L - 6 UNIDADES', cost_price: 48.54, sales_price: 53.94 },
+  { id: 11, code: 1010, name: 'KIT ROLO DE ALUMINIO + FILME PVC WYDA', cost_price: 8.80, sales_price: 9.78 },
+  { id: 12, code: 1020, name: 'SUPER PACK RED BULL VARIADOS - 6 UNIDADES', cost_price: 51.81, sales_price: 57.00 }
+];
+
+export default mock;
