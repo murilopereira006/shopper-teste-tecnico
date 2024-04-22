c17dd8699515e6fc4f09ab8245654d904d6ffe90
muriloPereira006 <muriloedu006@gmail.com>
2024-04-21 22:50:29 -0300

    add: router

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,98 +1,13 @@
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
+const products = require('./routes/products');
 require('dotenv').config();
 
-const prisma = new PrismaClient();
 const app = express();
 const PORT_BACKEND = process.env.PORT_BACKEND || 5000;
 
 app.use(express.json());
+app.use('/products', products);
 
-app.get('/products', async (req, res) => {
-  try {
-    const products = await prisma.products.findMany({});
-    res.json(products);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(`Erro ao consultar produtos. ${error}`);
-  }
-});
-
-app.get('/products/:id', async (req, res) => {
-  const productId = parseInt(req.params.id);
-  try {
-    const product = await prisma.products.findUnique({
-      where: {
-        id: productId
-      }
-    });
-    if (!product) {
-      return res.status(404).send('Produto não encontrado.');
-    }
-    res.json(product);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(`Erro ao consultar produto. ${error}`);
-  }
-});
-
-app.get('/products/by-code', async (req, res) => {
-  const productCode = parseInt(req.query.code);
-  try {
-    const product = await prisma.products.findFirst({
-      where: {
-        code: productCode
-      },
-      include: {
-        packs: true
-      }
-    });
-    if (!product) {
-      return res.status(404).send('Produto não encontrado.');
-    }
-    res.json(product);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Erro ao consultar produto.');
-  }
-});
-
-app.post('/products', async (req, res) => {
-  const { code, name, cost_price, sales_price, packs } = req.body;
-  try {
-    // Verificar se o código do produto já existe no banco de dados
-    const existingProduct = await prisma.products.findUnique({
-      where: {
-        code: code
-      }
-    });
-
-    if (existingProduct) {
-      // Se o código do produto já existir, retorne um erro
-      return res.status(400).send('Código de produto já está em uso.');
-    }
-
-    // Se o código do produto não existir, criar um novo registro
-    const product = await prisma.products.create({
-      data: {
-        code,
-        name,
-        cost_price,
-        sales_price,
-        packs: {
-          create: packs
-        }
-      },
-      include: {
-        packs: true
-      }
-    });
-    res.json(product);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(`Erro ao cadastrar produto. ${error}`);
-  }
-});
 
 app.listen(PORT_BACKEND, () => {
   console.log(`Example app is listening on port ${PORT_BACKEND}.`);
---
+++ backend/src/routes/products.js
@@ -0,0 +1,90 @@
+const { PrismaClient } = require('@prisma/client');
+const express = require("express");
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await prisma.products.findMany({});
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(`Erro ao consultar produtos. ${error}`);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const productId = parseInt(req.params.id);
+  try {
+    const product = await prisma.products.findUnique({
+      where: {
+        id: productId
+      }
+    });
+    if (!product) {
+      return res.status(404).send('Produto não encontrado.');
+    }
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(`Erro ao consultar produto. ${error}`);
+  }
+});
+
+router.get('/by-code', async (req, res) => {
+  const productCode = parseInt(req.query.code);
+  try {
+    const product = await prisma.products.findFirst({
+      where: {
+        code: productCode
+      },
+      include: {
+        packs: true
+      }
+    });
+    if (!product) {
+      return res.status(404).send('Produto não encontrado.');
+    }
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao consultar produto.');
+  }
+});
+
+router.post('/', async (req, res) => {
+  const { code, name, cost_price, sales_price, packs } = req.body;
+  try {
+    const existingProduct = await prisma.products.findUnique({
+      where: {
+        code: code
+      }
+    });
+
+    if (existingProduct) {
+      return res.status(400).send('Código de produto já está em uso.');
+    }
+
+    const product = await prisma.products.create({
+      data: {
+        code,
+        name,
+        cost_price,
+        sales_price,
+        packs: {
+          create: packs
+        }
+      },
+      include: {
+        packs: true
+      }
+    });
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(`Erro ao cadastrar produto. ${error}`);
+  }
+});
+
+module.exports = router;
