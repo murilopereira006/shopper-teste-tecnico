037e8b2063b4d6a8dc33282b193bcfc15aca792f
muriloPereira006 <muriloedu006@gmail.com>
2024-04-21 21:01:55 -0300

    add: function for xlsx

--- backend/src/controller/validations.js
+++ backend/src/controller/validations.js
@@ -1,3 +1,5 @@
+const xlsx = require('xlsx');
+
 const parseString = (inputString) => {
   const cleanedString = inputString.replace(/\r/g, '');
 
@@ -21,6 +23,28 @@ const readCSV = async (csvData) => {
   return parseString(csvData);
 };
 
-const readXLSX = () => { }
+const readXLSX = (xlsxData) => {
+  const workbook = xlsx.read(xlsxData, { type: 'buffer' });
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
+  const data = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+  const keys = data[0];
+  const objects = [];
+
+  for (let i = 1; i < data.length; i++) {
+    const values = data[i];
+    const obj = {};
+    for (let j = 0; j < keys.length; j++) {
+      if (keys[j] === 'code') {
+        obj[keys[j]] = String(values[j]);
+      } else {
+        obj[keys[j]] = values[j];
+      }
+    }
+    objects.push(obj);
+  }
+
+  return objects;
+};
 
 module.exports = { readCSV, readXLSX };
--- backend/src/test/validations.test.js
+++ backend/src/test/validations.test.js
@@ -10,9 +10,9 @@ test('Recebe arquivo .csv e retorna um Array de produtos em formato JSON', async
   expect(products).toEqual(mock);
 });
 
-// test('Recebe arquivo .xlsx e retorna um Array de produtos em formato JSON', () => {
-//   const xlsxFile = fs.readFileSync('./mock/shopper_products.xlsx')
-//   const products = readXLSX(xlsxFile) 
-//   expect(products).toBe(mock)
-// });
-
+test('Recebe arquivo .xlsx e retorna um Array de produtos em formato JSON', async () => {
+  const xlsxFilePath = path.join(__dirname, './mocks/xlsxdemo.xlsx');
+  const xlsxData = fs.readFileSync(xlsxFilePath);
+  const products = await readXLSX(xlsxData);
+  expect(products).toEqual(mock);
+});
